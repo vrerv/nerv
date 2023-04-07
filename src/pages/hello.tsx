2d65ba9297a9f21fde3327ddc495d01602095045
Soonoh Jung <soonoh.jung@gmail.com>
2023-04-07 09:10:29 +0900

    Init NextJS boilerplate code and add hello landing

---
+++ src/pages/hello.tsx
@@ -0,0 +1,79 @@
+
+import beachImg from '../../public/assets/images/DALL-E_A-tranquil-beach-with-turquoise-waters-and-powdery-white-sand-stretching-out-to-the-horizon.png'
+
+const Hello = () => {
+
+    return <>
+        <header className="bg-gray-900 text-white fixed top-0 w-full z-10 no-underline">
+            <nav className="container mx-auto flex justify-start">
+                <ul className="flex h-16 items-center">
+                    <li className="flex-grow flex-shrink"><a href="#hello" className="block px-4 py-2 hover:bg-gray-800 no-underline text-decoration-none">안녕하세요</a></li>
+                    <li className="flex-grow flex-shrink"><a href="#about" className="px-4 py-2 hover:bg-gray-800 no-underline text-decoration-none" >브레브는</a></li>
+                    <li className="flex-grow flex-shrink"><a href="#services" className="px-4 py-2 hover:bg-gray-800 no-underline text-decoration-none">도전</a></li>
+                    <li className="flex-grow flex-shrink"><a href="https://github.com/vrerv" className="block px-4 py-2 hover:bg-gray-800 no-underline text-white"><i className="fab fa-github mr-2"></i>GitHub</a></li>
+                    {/*
+                    <li className="flex-grow flex-shrink"><a href="#contact" className="px-4 py-2 hover:bg-gray-800 no-underline text-decoration-none">문의하기</a></li>
+                    */}
+                </ul>
+            </nav>
+        </header>
+        <main className="mt-16">
+            <section id="hello" className="bg-gray-100 py-12 h-96 items-center pt-16 hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-150">
+                <div className="container mx-auto px-4">
+                    <h1 className="text-4xl font-bold mb-4">브레브에 오신것을 환영합니다.</h1>
+                    <p className="mb-8">"브레브" 나의 인생아! 성공을 위한 파트너!</p>
+                    {/*
+                    <a href="#about" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">더 알아보기</a>
+                    */}
+                </div>
+            </section>
+            <section id="about" className="bg-gray-200 py-12 h-96 items-center pt-16 transition duration-300 ease-in-out transform hover:scale-150">
+                <div className="container mx-auto px-4">
+                    <h2 className="text-3xl font-bold mb-4">브레브에 대하여</h2>
+                    <p className="mb-8">
+                        브레브는 IT 세상에서 더 효율적인 방법을 찾습니다.<br/>
+                        기술을 인간중심으로 사용하고 싶습니다.<br/>
+                        개발자의 역량을 상향 평준화 시키고 싶습니다.<br/>
+                        인공지능이 모두에게 도움이 되는 방법을 찾고 싶습니다.<br/>
+                        모두 잘사는 사회를 위해 노력합니다.<br/>
+                        브레브! 성공한 당신을 위하여!
+                    </p>
+                </div>
+            </section>
+            <section id="services" className="bg-gray-100 py-0 h-96 items-center pt-16 bg-cover bg-center bg-no-repeat
+             transition duration-300 ease-in-out transform hover:scale-150"
+                     style={{ backgroundImage: `url(${beachImg})` }}>
+                <div className="container mx-auto px-4">
+                    <h2 className="text-3xl font-bold mb-4">준비중인 서비스</h2>
+                    <ul className="list-disc pl-4">
+                        <li>마인드맵 태스크 관리</li>
+                        <li>개발자 구독 서비스</li>
+                    </ul>
+                </div>
+            </section>
+            {/*
+            <section id="contact" className="bg-gray-200 py-12 h-screen items-center pt-16">
+                <div className="container mx-auto px-4">
+                    <h2 className="text-3xl font-bold mb-4">문의하기</h2>
+                    <form className="mb-8">
+                        <div className="mb-4">
+                            <label className="block text-gray-700 font-bold mb-2" htmlFor="name">이름:</label>
+                            <input className="border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Enter your name"/>
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 font-bold mb-2" htmlFor="email">이메일:</label>
+                            <input className="border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter your email"/>
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 font-bold mb-2" htmlFor="message">메세지:</label>
+                            <textarea className="border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="message" placeholder="Enter your message"></textarea>
+                        </div>
+                    </form>
+                </div>
+            </section>
+            */}
+        </main>
+    </>
+}
+
+export default Hello
