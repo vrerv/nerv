4151c884e86c4e35631434eee2990b714c6a1f17
Soonoh Jung <soonoh.jung@gmail.com>
2023-08-03 16:55:23 +0900

    add article - using obs as android emulator's camera

---
+++ src/components/Youtube.js
@@ -0,0 +1,17 @@
+/**
+ * Show embedded youtube video
+ * @param src - Use embed youtube url. e.g. https://www.youtube.com/embed/DjhW4nywf5o
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const Youtube = ({src}) => {
+  return (
+
+    <iframe src={src} style={{width: "100%", "aspect-ratio": "16/9"}}
+            title="YouTube video player" frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen></iframe>
+  );
+};
+
+export default Youtube;
