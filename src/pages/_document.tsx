a47e33d969112bc43353e5bb28113c4f85a90785
Soonoh Jung <soonoh.jung@gmail.com>
2023-08-28 16:59:53 +0900

    feat: add google analytics configuration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 import { AppConfig } from '@/utils/AppConfig';
+import Script from "next/script";
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
@@ -9,8 +10,17 @@ class MyDocument extends Document {
     return (
       <Html lang={AppConfig.locale}>
         <Head>
+          <Script strategy={"lazyOnload"} src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+          <Script>{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+          </Script>
         </Head>
-        <body>
+        <body style={{ overscrollBehavior: 'none' }}>
           <Main />
           <NextScript />
         </body>
