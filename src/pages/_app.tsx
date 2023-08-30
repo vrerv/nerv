93218ae9bcd935f261215c6b556ac8d9f9004c0a
Soonoh Jung <soonoh.jung@gmail.com>
2023-08-30 17:22:09 +0900

    chore: update google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,25 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 // @ts-ignore
 import { Mermaid } from '../components/Mermaid';
+import React from "react";
+import Script from "next/script";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
 
   <ThemeProvider attribute="class" defaultTheme="system">
     <Component {...pageProps} />
+    {/* Following nextjs doc - https://nextjs.org/docs/messages/next-script-for-ga */}
+    <div>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+      <Script id="google-analytics">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+      </Script>
+    </div>
   </ThemeProvider>
 );
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-import { AppConfig } from '@/utils/AppConfig';
-import Script from "next/script";
+import { AppConfig } from "@/utils/AppConfig";
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
@@ -10,15 +9,6 @@ class MyDocument extends Document {
     return (
       <Html lang={AppConfig.locale}>
         <Head>
-          <Script strategy={"lazyOnload"} src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
-          <Script>{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-          `}
-          </Script>
         </Head>
         <body style={{ overscrollBehavior: 'none' }}>
           <Main />
