06e57cf80daebbbaae2d17a889febb3199371111
Soonoh Jung <soonoh.jung@gmail.com>
2023-09-05 01:31:19 +0900

    feat: add daum web master tool verification

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   siteUrl: 'https://www.vrerv.com',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    // To add Daum WebMaster Tool
+    transformRobotsTxt: async (_, txt) => {
+      return txt + "\n\n" + "#DaumWebMasterTool:88be9da1200db897941f748c5f0c36e9bf866e88b0f33d5ca7a94666505550d1:IDkXKBkzqNtyNhqWHFndBw=="
+    }
+  }
 };
