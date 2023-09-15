8db00f4fb9e7071e8de1ad187f71cfdf438dbf81
Soonoh <soonoh.jung@gmail.com>
2023-09-15 17:56:25 +0900

    chore: update color selector

--- src/components/drawing/ColorSelector.js
+++ src/components/drawing/ColorSelector.js
@@ -1,75 +1,102 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const ColorSelector = ({selectedColor, setSelectedColor}) => {
 
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const colors = [
-      { name: 'Red', code: '#FF0000' },
-      { name: 'Green', code: '#008000' },
-      { name: 'Blue', code: '#0000FF' },
-      { name: 'Yellow', code: '#FFFF00' },
-      { name: 'Orange', code: '#FFA500' },
-      { name: 'Purple', code: '#800080' },
-      { name: 'Pink', code: '#FFC0CB' },
-      { name: 'Brown', code: '#A52A2A' },
-      { name: 'Black', code: '#000000' },
-      { name: 'White', code: '#FFFFFF' },
-      { name: 'Gray', code: '#808080' },
-      { name: 'Cyan', code: '#00FFFF' },
-    ];
+  const colors = [
+    { name: 'Red', code: '#FF0000' },
+    { name: 'Green', code: '#008000' },
+    { name: 'Blue', code: '#0000FF' },
+    { name: 'Yellow', code: '#FFFF00' },
+    { name: 'Orange', code: '#FFA500' },
+    { name: 'Purple', code: '#800080' },
+    { name: 'Pink', code: '#FFC0CB' },
+    { name: 'Brown', code: '#A52A2A' },
+    { name: 'Black', code: '#000000' },
+    { name: 'White', code: '#FFFFFF' },
+    { name: 'Gray', code: '#808080' },
+    { name: 'Cyan', code: '#00FFFF' },
+  ];
 
-    const toggleDropdown = () => {
-      setIsOpen(!isOpen);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (color) => {
+    setSelectedColor(color.name);
+    setIsOpen(false);
+  };
+
+  const [gridWidthPx, setGridWidthPx] = useState(100);
+  const [gridStyle, setGridStyle] = useState({});
+
+  useEffect(() => {
+    handleResize();
+
+    function handleResize() {
+      const { innerWidth, innerHeight } = window;
+      const ratio = innerWidth / innerHeight;
+      const columns = Math.ceil(Math.sqrt(colors.length * ratio));
+      const rows = Math.ceil(colors.length / columns);
+
+      setGridWidthPx(Math.floor((innerWidth - 100) / columns));
+
+      setGridStyle({
+        gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        gridTemplateRows: `repeat(${rows}, 1fr)`,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
+  }, []);
 
-    const handleSelect = (color) => {
-      setSelectedColor(color.name);
-      setIsOpen(false);
-    };
-
-    return (
-      <>
-        <style jsx>{`
-          .color-selector {
-            position: relative;
-          }
-
-          .dropdown-button {
-            cursor: pointer;
-          }
-
-          .dropdown-menu {
-            position: fixed; /* Changed from absolute to fixed */
-            top: 50%; /* Center vertically */
-            left: 50%; /* Center horizontally */
-            transform: translate(-50%, -50%); /* Necessary for precise centering */
-            border: 1px solid #ccc;
-            z-index: 1000; /* High z-index to make sure it's above other elements */
-          }
-
-          .dropdown-item {
-            padding: 8px;
-            cursor: pointer;
-            text-align: center;
-          }
-
-          .dropdown-item:hover {
-            opacity: 0.7;
-          }
-          `}</style>
-      <div className="color-selector" style={{ backgroundColor: selectedColor }} >
+  return (
+    <>
+      <style jsx>{`
+        .color-selector {
+          position: relative;
+        }
+        .dropdown-button {
+          cursor: pointer;
+        }
+        .dropdown-menu {
+          position: fixed;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          border: 1px solid #ccc;
+          z-index: 1000;
+          display: grid;
+        }
+        .dropdown-item {
+          padding: 8px;
+          cursor: pointer;
+          text-align: center;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        .dropdown-item:hover {
+          opacity: 0.7;
+        }
+      `}</style>
+      <div className="color-selector" style={{ backgroundColor: selectedColor }}>
         <button onClick={toggleDropdown} className="dropdown-button">
           {selectedColor}
         </button>
         {isOpen && (
-          <div className="dropdown-menu">
+          <div className="dropdown-menu" style={gridStyle}>
             {colors.map((color, index) => (
               <div
                 key={index}
                 onClick={() => handleSelect(color)}
                 className="dropdown-item"
-                style={{ backgroundColor: color.code }}
+                style={{ width: gridWidthPx, height: gridWidthPx, backgroundColor: color.code }}
               >
                 {color.name}
               </div>
@@ -77,8 +104,8 @@ const ColorSelector = ({selectedColor, setSelectedColor}) => {
           </div>
         )}
       </div>
-      </>
-    );
-  };
+    </>
+  );
+};
 
 export default ColorSelector;
