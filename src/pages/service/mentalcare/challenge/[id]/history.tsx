ed29c877818184926d3dd092da6493b412f804a5
Soonoh <soonoh.jung@gmail.com>
2023-11-17 17:26:21 +0900

    feat: add delete record

--- src/pages/service/mentalcare/challenge/[id]/history.tsx
+++ src/pages/service/mentalcare/challenge/[id]/history.tsx
@@ -13,6 +13,7 @@ import { GetStaticPaths } from "next";
 // @ts-ignore
 import { AskPicture } from '@/components/openai/ask-picture';
 import { MentalCareHeader } from "@/mentalcare/components/header";
+import { Button } from "@/components/ui/button";
 
 export async function getStaticProps({ locale }: { locale: any }) {
   return {
@@ -84,12 +85,24 @@ const IndexPage = ({locale}: { locale: string; }) => {
     }
   }, [challenge, recordMap])
 
+
+  const [editMode, setEditMode] = useState(false)
+
+  const handleEditMode = () => {
+    setEditMode(!editMode)
+  }
+
+  const handleRemoveRecord = (_recordId: number) => () => {
+    // TODO
+  }
+
   return challenge && <>
     <div className={'flex flex-col items-start p-0'}>
       <TabLayout control={
         () => <>
           <div className="p-2"><button onClick={handleList}>Back</button></div>
           <div className={"flex-grow"} />
+          <div className="p-2"><button onClick={handleEditMode}>{editMode ? 'Done' : 'Edit'}</button></div>
           <div className="p-2"><button onClick={handleWeeks}>Weeks</button></div>
         </>
       } >
@@ -101,7 +114,11 @@ const IndexPage = ({locale}: { locale: string; }) => {
                 <span className={"text-xl justify-end"}>{challenge.name}</span>
               </div>
               <br/>
-              {records.map(record => <div className={'font-mono'}>{record.action || '기록'} - {new Date(record.recordedAt).toLocaleString(locale)}</div>)}
+              {records.map(record =>
+                <div className={'font-mono'}>
+                  <span>{record.action || '기록'} - {new Date(record.recordedAt).toLocaleString(locale)}</span>{' '}
+                  {editMode && <Button type={'button'} variant={'outline'} size={'sm'} onClick={handleRemoveRecord(0 /* TODO */)}>Remove</Button>}
+                </div>)}
             </main>
           </div>
         </>
